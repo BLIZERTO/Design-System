5c6aaceaf3ccaace6f4230cb8e4bd373d165be98
Pierre Magnien <magniendcpierre@gmail.com>
2023-02-15 15:33:22 +0100

    font family

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styles/app.css';
 function App() {
   return (
     <div className="App">
-     <button type='submit' className={'btn'}></button>
+     <input type='text'  disabled className={'input'}></input>
     </div>
   );
 }

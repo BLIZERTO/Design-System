10d68d0022c7f6e5162a56f06a2fa1e994eb7b96
Thomas-Torrente <thomas.torrente@gmail.com>
2023-02-16 14:07:12 +0100

    add nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+/* Import Pages */
 import SignUp from "./pages/SignUp/SignUp";
 import SignIn from "./pages/SignIn/SignIn";
 import ForgetPassword from "./pages/ForgetPassword/ForgetPassword";
+/* Import css */
 import "./styles/app.css";
 
+/* Import Route */
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
+  const routerBrowser = createBrowserRouter([
+    /* Home / Signin */
+    {
+      path: "/",
+      element: <SignIn />,
+    },
+
+    /* Signup */
+    {
+      path: "/signup",
+      element: <SignUp />,
+    },
+
+    /* Forget Passwords */
+    {
+      path: "/forget-password",
+      element: <ForgetPassword />,
+    },
+  ]);
+
   return (
-    <div className="w-full h-full">
-      <ForgetPassword />
+    <div className="h-full w-full">
+      <RouterProvider router={routerBrowser} />
     </div>
   );
 }

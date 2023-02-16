326bff3bc8cf55f6869a228d8a5abd59de54c0ad
Thomas-Torrente <thomas.torrente@gmail.com>
2023-02-16 11:40:26 +0100

    Update Button

--- src/Molecules/Button.js
+++ src/Molecules/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 const Button = ({ text, onClick, style = "btn", icon = false, iconName }) => {
   /* si on passe icon à true lorsqu'on l'appel, la balise <i> s'affichera sinon non */
   return (
-    <button className={style} onClick={onClick}>
+    <button className={"btn " + style} onClick={onClick}>
       {text}
       {icon && <i className={iconName}></i>}
     </button>

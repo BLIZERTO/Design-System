e0d771fe87c317caf7463994b14df9abcad1ab91
Jérémy <jeremyschneider21@gmail.com>
2023-02-16 15:40:01 +0100

    move button in form

--- src/templates/AuthPage.js
+++ src/templates/AuthPage.js
@@ -4,12 +4,9 @@ function AuthPage(props) {
   return (
     <div className="flex flex-col gap-6 px-1 lg:px-0 lg:py-2 lg:w-1/3 mx-auto">
       <h1 className="text-2xl text-center">{props.title}</h1>
-      <form className="flex flex-col gap-4 lg:px-6">{props.children}</form>
-      <Button
-        type="submit"
-        text={props.btnText}
-        style="lg:w-1/3 mx-auto"
-      ></Button>
+      <form className="flex flex-col gap-4 lg:px-6">{props.children}
+        <Button type="submit" text={props.btnText} style="mx-auto w-full"></Button>
+      </form>
     </div>
   );
 }
